273d69adec232baf70e34106d88a7b7d0c7d224e
DESKTOP-88KRJV9\Togetaro <fcmaug@gmail.com>
2016-11-26 16:11:29 -0400

    adicionado export

---
+++ cursed/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1480190958,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/background.png",
+		"images/grama.png",
+		"images/bird-sheet0.png",
+		"images/cano-sheet0.png",
+		"images/canobaixo-sheet0.png",
+		"images/teto-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
